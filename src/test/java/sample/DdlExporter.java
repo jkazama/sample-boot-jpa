07ce05c8de96afef1a1bd439fb1741e54dc8c3ff
junichiro_kazama <junichiro_kazama@xon.jp>
2016-07-20 14:42:41 +0900

    DDL 出力の実装サポート

--- src/test/java/sample/DdlExporter.java
+++ src/test/java/sample/DdlExporter.java
@@ -2,15 +2,14 @@ package sample;
 
 import java.io.IOException;
 import java.nio.file.*;
+import java.util.Properties;
 
 import org.hibernate.boot.*;
-import org.hibernate.boot.model.naming.ImplicitNamingStrategyJpaCompliantImpl;
 import org.hibernate.boot.registry.*;
 import org.hibernate.boot.spi.MetadataImplementor;
 import org.hibernate.tool.hbm2ddl.SchemaExport;
+import org.springframework.boot.orm.jpa.hibernate.*;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-
-import sample.context.orm.DefaultRepository.*;
 
 /**
  * Entity 定義を元に DDL を生成します。  
@@ -25,60 +24,67 @@ import sample.context.orm.DefaultRepository.*;
  */
 public class DdlExporter {
 
-//    private static final String PackageRoot = "sample";
-//    private static final String PackageDefault = PackageRoot + ".model";
-//    private static final String PackageSystem = PackageRoot + ".context";
-//    private static final String OrmDialect = "org.hibernate.dialect.H2Dialect";
-//    private static final String OutputRoot = "build/";
-//
-//    public static void main(String[] args) {
-//        DdlExporter exporter = new DdlExporter();
-//        exporter.outputDdl(PackageSystem, OrmDialect, "ddl-system.sql");
-//        exporter.outputDdl(PackageDefault, OrmDialect, "ddl-default.sql");
-//    }
-//
-//    private void outputDdl(String packageName, String dialect, String fileName) {
-//        LocalSessionFactoryBean sfBean = sfBean(packageName, dialect);
-//        StandardServiceRegistry serviceRegistry = sfBean.getConfiguration().getStandardServiceRegistryBuilder().build();
-//        try {
-//            String outputFile = OutputRoot + fileName;
-//            Files.deleteIfExists(Paths.get(outputFile));
-//            MetadataImplementor metadata = metadata(sfBean);
-//            
-//            SchemaExport export = new SchemaExport(serviceRegistry, metadata, false);
-//            export.setDelimiter(";");
-//            export.setOutputFile(outputFile);
-//            export.create(true, false);
-//        } catch (Exception e) {
-//            throw new InvocationException(e);
-//        } finally {
-//            StandardServiceRegistryBuilder.destroy( serviceRegistry );
-//        }
-//    }
-//
-//    private LocalSessionFactoryBean sfBean(String packageName, String dialect) {
-//        DefaultRepositoryConfig config = new DefaultRepositoryConfig();
-//        config.setShowSql(false);
-//        config.setPackageToScan(packageName);
-//        config.setDialect(dialect);
-//        config.getProperties().put("hibernate.hbm2ddl.auto", "none");
-//        LocalSessionFactoryBean sfBean = config.sessionFactory(null, null);
-//        try {
-//            sfBean.afterPropertiesSet();
-//        } catch (IOException e) {
-//            throw new InvocationException(e);
-//        }
-//        return sfBean;
-//    }
-//    
-//    private MetadataImplementor metadata(LocalSessionFactoryBean sfBean) throws Exception {
-//        MetadataSources metadataSources = sfBean.getMetadataSources();
-//        Metadata metadata = metadataSources
-//                .getMetadataBuilder()
-//                .applyPhysicalNamingStrategy(new OrmNamingStrategy())
-//                .applyImplicitNamingStrategy(ImplicitNamingStrategyJpaCompliantImpl.INSTANCE)
-//                .build();
-//        return (MetadataImplementor) metadata;
-//    }
+    private static final String PackageRoot = "sample";
+    private static final String PackageDefault = PackageRoot + ".model";
+    private static final String PackageSystem = PackageRoot + ".context";
+    private static final String OrmDialect = "org.hibernate.dialect.H2Dialect";
+    private static final String OutputRoot = "build/";
+    private static final boolean FormatSql = false;
+
+    public static void main(String[] args) {
+        DdlExporter exporter = new DdlExporter();
+        exporter.outputDdl(PackageSystem, OrmDialect, "ddl-system.sql");
+        exporter.outputDdl(PackageDefault, OrmDialect, "ddl-default.sql");
+    }
+
+    private void outputDdl(String packageName, String dialect, String fileName) {
+        LocalSessionFactoryBean sfBean = sfBean(packageName, dialect);
+        StandardServiceRegistry serviceRegistry = sfBean.getConfiguration().getStandardServiceRegistryBuilder().build();
+        try {
+            String outputFile = OutputRoot + fileName;
+            Files.deleteIfExists(Paths.get(outputFile));
+            MetadataImplementor metadata = metadata(sfBean, serviceRegistry);
+            
+            SchemaExport export = new SchemaExport(serviceRegistry, metadata, false);
+            export.setDelimiter(";");
+            export.setFormat(FormatSql);
+            export.setOutputFile(outputFile);
+            export.create(true, false);
+        } catch (Exception e) {
+            throw new InvocationException(e);
+        } finally {
+            StandardServiceRegistryBuilder.destroy( serviceRegistry );
+        }
+    }
+
+    private LocalSessionFactoryBean sfBean(String packageName, String dialect) {
+        LocalSessionFactoryBean sfBean = new LocalSessionFactoryBean();
+        try {
+            sfBean.setPackagesToScan(packageName);
+            sfBean.setHibernateProperties(hibernateProperties(dialect));
+            sfBean.afterPropertiesSet();
+        } catch (IOException e) {
+            throw new InvocationException(e);
+        }
+        return sfBean;
+    }
+    
+    private Properties hibernateProperties(String dialect) {
+        Properties props = new Properties();
+        props.put("hibernate.dialect", dialect);
+        props.put("hibernate.show_sql", false);
+        props.put("hibernate.hbm2ddl.auto", "none");
+        return props;
+    }
+
+    private MetadataImplementor metadata(LocalSessionFactoryBean sfBean, StandardServiceRegistry registry) throws Exception {
+        MetadataSources metadataSources = sfBean.getMetadataSources();
+        Metadata metadata = metadataSources
+                .getMetadataBuilder(registry)
+                .applyPhysicalNamingStrategy(new SpringPhysicalNamingStrategy())
+                .applyImplicitNamingStrategy(new SpringImplicitNamingStrategy())
+                .build();
+        return (MetadataImplementor) metadata;
+    }
 
 }
